ebd04f5e1e0a13a4d4edaefaa639cfb133f8d23d
chizzycodhub <chukwudinwobodo01@gmail.com>
2024-07-28 07:07:07 -0700

    Modified to pass checks

--- 0x06-regular_expressions/1-repetition_token_0.rb
+++ 0x06-regular_expressions/1-repetition_token_0.rb
@@ -1,4 +1,4 @@
 #!/usr/bin/env ruby
-# a Ruby script that accepts one argument and pass it to a regular expression matching method to repeat a token zero or more times
+# a Ruby script that accepts one argument and pass it to a regular expression matching method to repeat a token
 
-puts ARGV[0].scan(/hbt*n/).join
+puts ARGV[0].scan(/hbt{2,5}n/).join
