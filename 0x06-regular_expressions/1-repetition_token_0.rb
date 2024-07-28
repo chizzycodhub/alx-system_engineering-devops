df094f09ec396a2adcb25454d2dcfdb0299c48c3
chizzycodhub <chukwudinwobodo01@gmail.com>
2024-07-28 06:47:13 -0700

    A regular expression that repeats a token zero or more times

--- 0x06-regular_expressions/1-repetition_token_0.rb
+++ 0x06-regular_expressions/1-repetition_token_0.rb
@@ -1,4 +1,4 @@
 #!/usr/bin/env ruby
-# a Ruby script that accepts one argument and pass it to a regular expression matching method to repeat a token not less than 2 times and not more than 5 times
+# a Ruby script that accepts one argument and pass it to a regular expression matching method to repeat a token zero or more times
 
-puts ARGV[0].scan(/hbt{2,5]n/).join
+puts ARGV[0].scan(/hbt*n/).join
